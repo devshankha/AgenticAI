17415c320b4a3a9f2a218ebb03b31ce78b83f1ef
devshankha <45039485+devshankha@users.noreply.github.com>
2026-06-14 19:59:38 +0530

    Update AgentSetUp11.py

--- AgentSetUp11.py
+++ AgentSetUp11.py
@@ -1,11 +1,6 @@
 from dotenv import load_dotenv
-from openai import OpenAI
-from langchain_core.tools import Tool
-from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
 from langchain_classic.chains import create_retrieval_chain
-from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 
 # Load environment variables (e.g., OpenAI API keys) from a .env file located on the local system
@@ -18,7 +13,7 @@ prompt_template = PromptTemplate(
 
 # Generate the prompt by filling in the variable
 formatted_prompt = prompt_template.format(name="David")
-print(formatted_prompt) 
+print(formatted_prompt)
 chat_model = ChatOpenAI(model="gpt-3.5-turbo")
 
 # Send a message to the model and get the response
